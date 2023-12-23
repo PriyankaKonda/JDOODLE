b4c53e2f43526f1b4fd5fa8bb4c1b83fe0a873c6
PriyankaKonda <priyanka.kpk22@gmail.com>
2023-12-23 21:02:41 +0530

    Update test-doodle.spec.js
    committing to main

--- tests/test-doodle.spec.js
+++ tests/test-doodle.spec.js
@@ -25,7 +25,7 @@ customtest('Should Display Sections', async ({ page, Sections }) => {
   await expect(texts).toEqual(Sections)
 });
 
-
+//Note: This test is failing because of the captcha issues
 test('Fetching the program values and Executing the program', async ({ page }) => {
   let Inputs;
   await test.step('Fetching the program values', async () => {
